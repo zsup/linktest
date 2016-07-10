43531b6daaf002ba544c503fbf5da5cada23592d
Zach Supalla <zsupalla@gmail.com>
2016-07-10 14:26:57 -0700

    Added README and .gitignore

--- linktest.go
+++ linktest.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "os"
+  "github.com/urfave/cli"
+)
 
 func main() {
-  fmt.Println("Hello world!")
+  app := cli.NewApp()
+  app.Name = "linktest"
+  app.Usage = "Crawl a website, searching for broken links and imgs."
+  app.Action = func(c *cli.Context) error {
+    fmt.Printf("Hello %q", c.Args().First())
+    return nil
+  }
+
+  app.Run(os.Args)
 }
